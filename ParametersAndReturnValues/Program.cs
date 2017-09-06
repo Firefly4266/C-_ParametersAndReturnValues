b69246ba7444ea3c6203ddf36b49ead4a89aa1c7
Firefly4266 <gaines.johnp@gmail.com>
2017-09-05 21:09:54 -0700

    Simple calc exercise completed.

--- ParametersAndReturnValues/Program.cs
+++ ParametersAndReturnValues/Program.cs
@@ -10,6 +10,17 @@ namespace ParametersAndReturnValues
     {
         static void Main(string[] args)
         {
+            SimpleCalculator calc = new SimpleCalculator();
+
+            Console.WriteLine(calc.Square(4));
+            Console.WriteLine(calc.AddTwoNumbers(5, 6));
+            Console.WriteLine("Enter a number to be squared.  ");
+            string input = Console.ReadLine();
+            int userRes = int.Parse(input);
+            Console.WriteLine("The suqare of {0} is {1}", userRes, calc.Square(userRes));
+            Console.WriteLine();
+            Console.WriteLine("The maximum value is {0}.", calc.FindMaxOfThree(21, 58, 32));
+            Console.WriteLine("figure that one out...");
         }
     }
 }
